245e9b765c803522ebfd3b621f529f72521c07f6
joechea <s.chea@aupp.edu.kh>
2024-07-09 17:07:57 +0700

    add hidden file to search directory
    hide venv from telescope search result

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -403,7 +403,17 @@ require('lazy').setup {
           -- mappings = {
           --   i = { ['<c-enter>'] = 'to_fuzzy_refine' },
           -- },
-          file_ignore_patterns = { 'node_modules', '.git', '.vscode', '.cache' },
+          vimgrep_arguments = {
+            'rg',
+            '--color=never',
+            '--no-heading',
+            '--with-filename',
+            '--line-number',
+            '--column',
+            '--smart-case',
+            '--hidden', -- Include hidden files
+          },
+          file_ignore_patterns = { 'node_modules', '.git', '.vscode', '.cache', 'venv' },
         },
         -- pickers = {}
         extensions = {
@@ -421,7 +431,11 @@ require('lazy').setup {
       local builtin = require 'telescope.builtin'
       vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = '[S]earch [H]elp' })
       vim.keymap.set('n', '<leader>sk', builtin.keymaps, { desc = '[S]earch [K]eymaps' })
-      vim.keymap.set('n', '<leader>sf', builtin.find_files, { desc = '[S]earch [F]iles' })
+      vim.keymap.set('n', '<leader>sf', function()
+        builtin.find_files {
+          hidden = true,
+        }
+      end, { desc = '[S]earch [F]iles' })
       vim.keymap.set('n', '<leader>ss', builtin.builtin, { desc = '[S]earch [S]elect Telescope' })
       vim.keymap.set('n', '<leader>sw', builtin.grep_string, { desc = '[S]earch current [W]ord' })
       vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = '[S]earch by [G]rep' })
